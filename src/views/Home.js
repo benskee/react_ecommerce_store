e83e0ec49af13c6cb28f36ff447ded150a088f16
benskee <benskee33@gmail.com>
2021-03-19 09:08:28 -0400

    init

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div className='md-4 offset-4'>
+                    <h4>This is the home page!</h4>
+                </div>
+            </div>
+        )
+    }
+}
